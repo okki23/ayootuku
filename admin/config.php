08bfde7ebed8713bcfc4db2181ea209b513d1da0
okki23 <okkisetyawan@gmail.com>
2022-05-31 08:21:53 +0700

    update first

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Admin');
+
+// HTTP
+define('HTTP_SERVER', 'http://www.asiamegatama-store.local/admin-toko/');
+define('HTTP_CATALOG', 'http://www.asiamegatama-store.local/');
+
+// DIR
+define('DIR_OPENCART', '/var/www/html/oc/');
+define('DIR_APPLICATION', DIR_OPENCART . 'admin-toko/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_STORAGE', '/var/www/html/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'okki');
+define('DB_PASSWORD', 'kecepatan93');
+define('DB_DATABASE', 'db_asiamegatama');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
